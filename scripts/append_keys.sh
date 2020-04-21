30cddf8efd09b02275a5492a9fc181989f968dbd
Richard Chen Zheng <58443436+rchenzheng@users.noreply.github.com>
2020-04-21 17:37:51 -0400

    Add basic help with no arguments

--- scripts/append_keys.sh
+++ scripts/append_keys.sh
@@ -2,8 +2,15 @@
 # Encode and append keys
 keys=($(ls *.pem))
 
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "$0 filename"
+    exit 1
+fi
+
 for k in ${keys[@]}; do
 
   encoded=$(cat ${k} | base64) 
-  echo $k: ${encoded} >> db-ssl-certs.yml 
+  echo "  $k: ${encoded}" >> $1 
 done

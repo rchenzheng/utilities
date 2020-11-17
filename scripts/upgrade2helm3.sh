a5d3b474c74c45e6e34dd1c967b2bf722828063e
Richard Chen Zheng <58443436+rchenzheng@users.noreply.github.com>
2020-11-17 18:23:27 -0500

    Add helm upgrade script

---
+++ scripts/upgrade2helm3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# exit on any error
+set -euo pipefail
+
+IFS=" " read -r -a releases <<< "$(helm list --short)"
+
+for release in "${releases[@]}"; do
+	helm3 2to3 convert "$release" --tiller-out-cluster
+done
+
+# show releases across all namespaces
+helm3 list -A
+
+echo '### Ensure all releases are listed in HELM3 releases before cleanup ```helm3 2to3 cleanup --tiller-out-cluster```###'
